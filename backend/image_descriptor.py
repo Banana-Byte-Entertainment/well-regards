32e198d0435a0cbc556cb3da71d49cbbef828e33
jo.su <chrsut2005@gmail.com>
2025-11-16 19:20:24 -0800

    IMAGE DESCRIPTION WORKING

--- backend/image_descriptor.py
+++ backend/image_descriptor.py
@@ -1,3 +1,28 @@
+from backend.read_image import ObjectDetector
+
 class ImageDescriptor:
+<<<<<<< Updated upstream
   def describeImage(self, img):
     return []
+=======
+  # takes in PATH to image, returns list of detected objects
+  def describeImage(self, img_path):
+    img_reader = ObjectDetector(confidence_threshold=0.7)
+    object_names = []
+    for detection in img_reader.detect_objects(img_path):
+      object_names.append(detection['class'])
+    return object_names
+  
+  # takes in array of string and returns a string
+  # "This image contains: object1, ..., objectN."
+  def array_to_description(self, arr):
+    result = "This image contains: "
+    
+    if len(arr) == 0:
+      return "This image contains nothing." 
+
+    result += ", ".join(arr)
+    result += "."
+
+    return result
+>>>>>>> Stashed changes
--- backend/tests/test_image_description.py
+++ backend/tests/test_image_description.py
@@ -3,4 +3,21 @@ from backend.image_descriptor import ImageDescriptor
 def test_describe_image():
   descriptor = ImageDescriptor()
   result = descriptor.describeImage('./backend/tests/cat.jpg')
+<<<<<<< Updated upstream
   assert 'cat' in result
+=======
+  assert 'cat' in result
+  
+def test_array_to_description():
+  descriptor = ImageDescriptor()
+  result = descriptor.array_to_description([])
+  assert 'This image contains nothing' in result
+  result = descriptor.array_to_description(['cat'])
+  assert 'This image contains: cat.' in result
+  result = descriptor.array_to_description(['cat', 'dog'])
+  assert 'This image contains: cat, dog.' in result
+  descriptor = ImageDescriptor()
+  image_description = descriptor.describeImage('./backend/tests/cat.jpg')
+  result = descriptor.array_to_description(image_description)
+  assert 'This image contains: cat.' in result
+>>>>>>> Stashed changes
